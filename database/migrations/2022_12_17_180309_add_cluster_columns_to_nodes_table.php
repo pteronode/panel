872ca6a2cf2bb709a5edd8e0d6baab2df98d9d98
Andrei-Lucian Mociu <mociu.andrei@gmail.com>
2022-12-18 12:39:31 +0000

    Encrypter

--- database/migrations/2022_12_17_180309_add_cluster_columns_to_nodes_table.php
+++ database/migrations/2022_12_17_180309_add_cluster_columns_to_nodes_table.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Container\Container;
+use Illuminate\Contracts\Encryption\Encrypter;
 
 return new class extends Migration
 {
